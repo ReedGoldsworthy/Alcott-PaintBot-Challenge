9f5597e2d66b37aa55808bf73355ed6b9b2b620c
MWyb24 <matthewwybraniec@gmail.com>
2026-04-23 14:13:22 -0500

    Update blue.js

--- blue.js
+++ blue.js
@@ -1,9 +1,8 @@
 // ──────────────────────────────────────────────
-//  BLUE TEAM BOT — edit this file and commit it!
+//  BLUE TEAM BOT — edit this file and commit it
 // ──────────────────────────────────────────────
 //
 //  Starting position: top-right corner, facing South
 //
 // TO DO
 // YOUR CODE HERE!
-
